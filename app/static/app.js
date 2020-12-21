a432354d5be5b52dcfd3940486b8138bce88b609
leshawn-rice <leshawnrice3@gmail.com>
2020-12-20 23:14:55 -0800

    removed disabling of add league button after submit cause it broke the app

--- app/static/app.js
+++ app/static/app.js
@@ -145,10 +145,8 @@ async function addNewLeagueLoadingScreen() {
   */
   const $content = $('.container-fluid');
   const $addLeagueForm = $('#add-league-form');
-  const $addLeagueBtn = $('#add-league-btn');
   $addLeagueForm.submit(async (event) => {
     event.preventDefault();
-    $addLeagueBtn.attr('disabled', 'disabled');
     const leagueId = $addLeagueForm.find('input[name="league_id"]').val()
     const year = $addLeagueForm.find('input[name="year"]').val()
     const $loadingRow = $('<div class="row">');
